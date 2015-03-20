4bcf7807c6442aa7a6f9ee444783e73e881a9957
landongn <landongn@gmail.com>
2015-03-19 21:36:47 -0700

    revising compilers

--- index.js
+++ index.js
@@ -1,34 +1,60 @@
-'use strict';
+
+
+/*
+	@landongn - shamelessly stolen from ember-cli-less
+ */
+
+var LESSCompiler = require('broccoli-less-single');
+var path         = require('path');
+var mergeTrees   = require('broccoli-merge-trees');
+
+function EmberTableLESSCompiler(options) {
+	this.name = 'ember-cli-less';
+	this.ext = 'less';
+	this.options = options;
+}
+
+EmberTableLESSCompiler.prototype.toTree = function(tree, inputPath, outputPath, options) {
+	options = options || {};
+	var ext = this.ext;
+	var paths = options.outputPaths || { app: options.registry.app.options.outputPaths.app.css };
+
+	var trees = Object.keys(paths).map(function(file) {
+		var input = path.join(inputPath, file + '.' + ext);
+		var output = paths[file];
+
+		return new LESSCompiler([tree], input, output, options);
+	});
+
+	// continue to mutate trees here, i'd reckon.
+	// (just return a new thing that returns a tree)
+
+
+	return mergeTrees(trees);
+};
 
 module.exports = {
-  name: 'ember-table',
+	name: 'ember-table',
 
-  included: function(app) {
-    this._super.included(app);
+	included: function(app) {
 
-    app.import(app.bowerDirectory + '/antiscroll/antiscroll.js');
-    app.import(app.bowerDirectory + '/antiscroll/antiscroll.css');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/jquery-ui.custom.js');
-    app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
+		app.import(app.bowerDirectory + '/antiscroll/antiscroll.js');
+		app.import(app.bowerDirectory + '/antiscroll/antiscroll.css');
+		app.import(app.bowerDirectory + '/jquery-ui/ui/jquery-ui.custom.js');
+		app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
 
-    // FIXME(azirbel): Need to import ember table CSS
-  },
+		var options = app.options.emberTableOptions || {};
+		  if ((options.sourceMap === undefined) && (app.env === 'development')) {
+		    options.sourceMap = true;
+		  }
+		  app.registry.add('css', new EmberTableLESSCompiler(options));
 
-  setupPreprocessorRegistry: function(type, registry) {
-    var tool = 'ember-cli-less';
-    console.log('setupPreprocessorRegistry using ' + tool);
-    // Also not working
-    // registry.add('css', 'broccoli-less', ['less']);
-    registry.add('css', {
-      name: tool,
-      ext: 'less',
-      toTree: function(tree) { return tree; }
-    });
-  },
+		// FIXME(azirbel): Need to import ember table CSS
+	},
 
-  afterInstall: function() {
-    this.addBowerPackageToProject('antiscroll');
-    this.addBowerPackageToProject('jquery-mousewheel');
-    this.addBowerPackageToProject('jquery-ui');
-  }
+	afterInstall: function() {
+		this.addBowerPackageToProject('antiscroll');
+		this.addBowerPackageToProject('jquery-mousewheel');
+		this.addBowerPackageToProject('jquery-ui');
+	}
 };
